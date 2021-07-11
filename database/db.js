5e441017801c65af15c7464e004cbdd539e8b1b7
Manuelomar <zaimon321@gmail.com>
2021-07-11 12:05:51 -0400

    primeros archivos del projecto

---
+++ database/db.js
@@ -0,0 +1,15 @@
+const mysql= require('mysql');
+const conexion = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password:'',
+  database:'crud_nodejs_db1' 
+});
+conexion.connect((error)=>{
+  if(error){
+    console.error('El error de conexion es: '+error)
+    return
+  }
+  console.log(' !Conectado a la BD MySQL');
+})
+module.exports= conexion;
